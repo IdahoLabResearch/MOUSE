f9df6a8887d2f30e145aab597037afc7d948047f
Gyutae Park <parkg2@inl.gov>
2025-06-24 09:49:54 -0600

    Update input with GCMR v0 information (incomplete)

--- watts_exec_GCMR.py
+++ watts_exec_GCMR.py
@@ -39,7 +39,6 @@ time_start = time.time()  # Store the current time to track the script's executi
 # Initialize user parameters using the watts library
 params = watts.Parameters()
 
-
 # **************************************************************************************************************************
 #                                                Sec. 1 : User Configuration
 # **************************************************************************************************************************
@@ -67,22 +66,20 @@ params['simplified_chain_thermal_xml'] =\
 # but the new material has to be included in the materials database at "core_design/openmc_materials_database"
 params['reactor type'] = "GCMR"
 params['TRISO Fueled'] = "Yes" 
+
 # The fuel and its properties
 params['fuel'] = 'un'
 params['enrichment'] = 0.19 # The enrichment is a fraction. It has to be between 0 and 1
+
 # Mixing UO2 and UC by atom fraction
 params['UO2 atom fraction'] = 0.7
 params['Reflector'] = 'BeO'
 params['Moderator'] = 'Graphite'
-params['moderator_booster'] = 'YHx' 
+params['moderator_booster'] = 'ZrH' 
 params['Coolant'] = 'Helium'
 
 # Temperature assumed for the OpenMC calculations
 params['common_temperature'] = 850 # Kelvins
-
-params['Control Drum Absorber'] = 'B4C_natural' # The absorber material in the control drums
-params['Control Drum Reflector'] = 'BeO'   # The reflector material in the control drums  
-
 
 # **************************************************************************************************************************
 #                                           Sec. 3 : User-Defined Parameters (Geometry: Fuel Pins, Moderator Pins, Coolant)
@@ -90,17 +87,18 @@ params['Control Drum Reflector'] = 'BeO'   # The reflector material in the contr
 
 # The reactor core includes fuel pins and moderator pins
 # The fuel pin is assumed to include 5 regions: kernel, buffer, 3 layers of ceramics
-
 # The materials of the 5 regions of the fuel pin
 params['fuel_pin_materials'] = ['un', 'buffer', 'PyC', 'SiC', 'PyC']
+
 # The outer radii of each of the 5 regions of the fuel pin
 params['fuel_pin_radii'] = [0.025, 0.035, 0.039, 0.0425, 0.047] # cm
 
 
-#A background material that is used anywhere within the lattice but outside a TRISO particle
+# A background material that is used anywhere within the lattice but outside a TRISO particle
 # The background material (params['matrix material']) is within the compact region where the TRISO particles are packed.
 # The moderator (params['moderator']) is outside this compact region but within the larger cylindrical fuel region.
 params['matrix material'] = 'Graphite'
+
 # The dimensions of the cylinderical lattice containing TRISO particles
 params['lattice height'] = 4 # cm
 
@@ -109,16 +107,15 @@ params['lattice radius'] = 0.6225 # cm
 params['lattice_compact_volume'] = cylinder_volume(params['lattice radius'],params['lattice height'])
 params['packing factor'] = 0.3 
 
+# Coolant Channel
 params['coolant channel radius'] = 0.35 #cm
 params['booster radius'] =  0.55 # cm
 
-
 # Hexagonal Lattice
 params['hex lattice_radius']  = 1.3 #cm
+
 # the distance between the centers of adjacent hexagons within the lattice structure
 params['lattice_pitch'] =  params['hex lattice_radius']*np.sqrt(3)
-
-
 
 # **************************************************************************************************************************
 #                                           Sec. 4 : User-Defined Parameters (Hexagonal Lattice)
@@ -126,26 +123,29 @@ params['lattice_pitch'] =  params['hex lattice_radius']*np.sqrt(3)
 
 # The number of rings (pins) along the edge of the hexagonal lattice
 params['assembly_rings'] = 6
+
 # the height of the hexagonal of one fuel assembly
 params['assembly_ftf'] = params['lattice_pitch']*(params['assembly_rings']-1)*np.sqrt(3)
 
 # #The thickness of the reflector around the lattice
 params['core_rings'] = 5
+
 # extra thickness for the reflector beyond the control drums
 params['extra_reflector'] = params['assembly_ftf'] / 10 #cm
 params['core_radius'] = params['assembly_ftf']* params['core_rings'] +  params['extra_reflector']
 params['active_height'] = 2 * params['core_radius']
 
-
-
 # **************************************************************************************************************************
 #                                           Sec. 5 : User-Defined Parameters (Control Drums)
 # ************************************************************************************************************************** 
 
+# Each of the control drums includes a reflector material (most of the drum) and an absorber material (the outer layer)
+params['Control Drum Reflector'] = 'BeO'   # The reflector material in the control drums  
+params['Control Drum Absorber'] = 'B4C_enriched' # The absorber material in the control drums
+params['Control Drum Count'] = 24
 params['Drum_Radius'] = 9 #cm  
 
-# # Each of the control drums includes a reflector material (most of the drum) and an absorber material (the outer layer)
-# # The thickness of the absorber layer
+# The thickness of the absorber layer
 params['drum_Absorber_thickness'] = 1 # cm
 
 # # If there are two drums alone each edge of the hexagonal lattice, the angle between drums pairs will be 60 degrees
@@ -167,7 +167,6 @@ params['all_drums_volume'], params['Control Drum Absorber Mass'], params['Contro
         params['drum_Absorber_thickness'], params)
 
 params['tot_drum_area_all'] = params['all_drums_volume'] / params['drum_height']
-
 
 params['Reflector Mass'] = calculate_reflector_mass_GCMR(params)          
 params['Moderator Mass'], params['Moderator Booster Mass'] = calculate_moderator_mass_GCMR(params) 
@@ -222,7 +221,6 @@ params['mass_U238'] = 263372.87  # grams
 params['Uranium Mass'] = (params['mass_U235'] + params['mass_U238']) / 1000 # Kg
 
 
-
 # **************************************************************************************************************************
 #                                           Sec. 7 : Fuel Calcs
 # ************************************************************************************************************************** 
@@ -244,14 +242,11 @@ params['Primary Loop Compressor Power'] = calculate_circulator_mechanical_power(
 # **************************************************************************************************************************
 #                                           Sec. 8 : Shielding
 # ************************************************************************************************************************** 
-
-# #Shielding
 # There is no in-vessel sheilding thickness
 params['in_vessel_shield_thickness'] = 0 #cm
 params['in_vessel_shielding_inner_radius'] = params['core_radius'] 
 params['in_vessel_shielding_outer_radius'] = params['core_radius'] + params['in_vessel_shield_thickness']
 params['In Vessel Shielding Material'] = 'B4C_natural'
-
 
 params['out_of_vessel_shield_thickness'] = 39.37 #cm
 params['Out Of Vessel Shielding Material'] = 'WEP'
@@ -272,8 +267,8 @@ params['vessel_bottom_depth'] = 35  # cm
 params['vessel_material'] ='stainless_steel'
 
 # assuming that no guard vessel 
-params['gap_between_vessel_and_guard_vessel'] = 0 # cm
-params['guard_vessel_thickness'] = 0
+params['gap_between_vessel_and_guard_vessel'] = 2 # cm
+params['guard_vessel_thickness'] = 5
 params['guard_vessel_material'] ='stainless_steel'
 
 params['gap_between_guard_vessel_and_cooling_vessel'] = 5 # cm
@@ -292,7 +287,6 @@ params['vessels_total_radius'], params['vessel_height'] , params['vessels_total_
 params['In Vessel Shielding Mass'] = cylinder_annulus_mass(params['in_vessel_shielding_outer_radius'],\
     params['in_vessel_shielding_inner_radius'], params['vessel_height'], params['In Vessel Shielding Material'] )  
 
-
 params['Out Of Vessel Shielding Mass'] = params['out_vessel_shield_effective_density_factor'] * cylinder_annulus_mass(params['out_of_vessel_shield_thickness']+ params['vessels_total_radius'],\
         params['out_of_vessel_shield_thickness'], params['vessels_total_height'], params['Out Of Vessel Shielding Material']) 
 params ['Vessel and Guard Vessel Masses'] = params['Vessel Mass'] +  params['Guard Vessel Mass']
@@ -304,11 +298,11 @@ params ['Vessel and Guard Vessel Masses'] = params['Vessel Mass'] +  params['Gua
 params['Operation Mode'] = "Autonomous" # "Non-Autonomous" or "Autonomous"
 params['Number of Operators'] = 2
 
-params['Levelization Period'] = 60 # in years
-params['Refueling Period'] = 7
+params['Levelization Period'] = 60 # [yr]
+params['Refueling Period'] = 7 # [d]
 params['Emergency Shutdowns Per Year']= 0.2
 
-params['Startup Duration after Refueling'] = 2
+params['Startup Duration after Refueling'] = 2 # [d]
 params['Startup Duration after Emergency Shutdown'] = 14
 params['Reactors Monitored Per Operator'] = 10
 params['Security Staff Per Shift'] = 1
@@ -322,17 +316,16 @@ params['Security Staff Per Shift'] = 1
 # A conservative estimate for the land area 
 # Ref: McDowell, B., and D. Goodman. "Advanced Nuclear Reactor Plant Parameter Envelope and
 #Guidance." National Reactor Innovation Center (NRIC), NRIC-21-ENG-0001 (2021). 
-
 params['Land Area'] = 18 # acres
-params['escalation_year'] = 2023
+params['escalation_year'] = 2024
 # excavation volume needs to be detailed
 params['Excavation Volume'] = 463.93388 # m3 
+
 # Financing params
 # Financing params
 params['Interest Rate'] = 0.065 # 
 params['Construction Duration'] = 12 # months 
 params['Debt To Equity Ratio'] = 0.5 
-
 params['Annual Return'] = 0.0475
 
 params['Reactor Building Slab Roof Volume'] = 219.18168 # m^3
@@ -350,27 +343,26 @@ params['Control Building Basement Volume'] = 27 # m^3
 params['Control Building Exterior Walls Volume'] = 19.44 # m^3
 
 # Refueling building 
-params['Refueling Building Slab Roof Volume'] = 312 # m^3
-params['Refueling Building Basement Volume'] = 312 # m^3
-params['Refueling Building Exterior Walls Volume'] = 340 # m^3
+params['Refueling Building Slab Roof Volume'] = 0 # m^3
+params['Refueling Building Basement Volume'] = 0 # m^3
+params['Refueling Building Exterior Walls Volume'] = 0 # m^3
 
 # spent fuel building
-params['Spent Fuel Building Slab Roof Volume'] = 384 # m^3
-params['Spent Fuel Building Basement Volume'] = 384
-params['Spent Fuel Building Exterior Walls Volume'] = 448
+params['Spent Fuel Building Slab Roof Volume'] = 0 # m^3
+params['Spent Fuel Building Basement Volume'] = 0
+params['Spent Fuel Building Exterior Walls Volume'] = 0
 
-params['Emergency Building Slab Roof Volume'] =  128
-params['Emergency Building Basement Volume'] = 128
-params['Emergency Building Exterior Walls Volume'] = 180
+params['Emergency Building Slab Roof Volume'] =  0
+params['Emergency Building Basement Volume'] = 0
+params['Emergency Building Exterior Walls Volume'] = 0
 
+params['Storage Building Slab Roof Volume'] =  0
+params['Storage Building Basement Volume'] =  0
+params['Storage Building Exterior Walls Volume'] =  0
 
-params['Storage Building Slab Roof Volume'] =  180
-params['Storage Building Basement Volume'] =  180
-params['Storage Building Exterior Walls Volume'] =  246.4
-
-params['Radwaste Building Slab Roof Volume'] =  280
-params['Radwaste Building Basement Volume'] =  280
-params['Radwaste Building Exterior Walls Volume'] =  358
+params['Radwaste Building Slab Roof Volume'] =  0
+params['Radwaste Building Basement Volume'] =  0
+params['Radwaste Building Exterior Walls Volume'] =  0
 
 # **************************************************************************************************************************
 #                                           Sec. 12 : Cost
