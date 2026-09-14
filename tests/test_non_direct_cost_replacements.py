5daab93333b08270503201e5910bc4ce68e1ea7d
BotrosHanna-INL <botros.naseif@gmail.com>
2026-09-14 13:22:59 -0600

    Break out annualized plant maintenance accounts

--- tests/test_non_direct_cost_replacements.py
+++ tests/test_non_direct_cost_replacements.py
@@ -6,7 +6,7 @@ from cost.non_direct_cost import calculate_accounts_31_32_75_82_cost
 
 
 class ReplacementAccountTests(unittest.TestCase):
-    def test_moderator_booster_is_separate_from_misc_replacements(self):
+    def test_replacements_and_maintenance_are_separate(self):
         foak = 'FOAK Estimated Cost ($/unit)'
         noak = 'NOAK Estimated Cost ($/unit)'
         account_costs = {
@@ -24,6 +24,22 @@ class ReplacementAccountTests(unittest.TestCase):
             221.31: 25.0,
             221.33: 50.0,
             221.34: 150.0,
+            212: 100.0,
+            213: 50.0,
+            214: 50.0,
+            215: 50.0,
+            222.1: 30.0,
+            222.2: 20.0,
+            222.3: 20.0,
+            222.6: 30.0,
+            223: 10.0,
+            221.11: 10.0,
+            226: 10.0,
+            228: 10.0,
+            227: 5.0,
+            23: 2.0,
+            24: 2.0,
+            26: 1.0,
             751: 0.0,
             752: 0.0,
             753: 0.0,
@@ -31,6 +47,14 @@ class ReplacementAccountTests(unittest.TestCase):
             755: 0.0,
             757: 0.0,
             759: 0.0,
+            759.1: 0.0,
+            759.2: 0.0,
+            759.3: 0.0,
+            759.4: 0.0,
+            759.5: 0.0,
+            759.6: 0.0,
+            759.7: 0.0,
+            759.8: 0.0,
         }
         data = pd.DataFrame({
             'Account': list(account_costs),
@@ -57,12 +81,22 @@ class ReplacementAccountTests(unittest.TestCase):
             booster = result.loc[
                 result['Account'] == 757, estimated_cost_column
             ].iat[0]
-            miscellaneous = result.loc[
+            maintenance = result.loc[
+                result['Account'].isin([
+                    759.1, 759.2, 759.3, 759.4,
+                    759.5, 759.6, 759.7, 759.8,
+                ]),
+                estimated_cost_column,
+            ].tolist()
+            maintenance_total = result.loc[
                 result['Account'] == 759, estimated_cost_column
             ].iat[0]
             self.assertEqual(booster, 150.0)
-            # 221.34 is removed from the residual: (1000 - 600) * 1%.
-            self.assertEqual(miscellaneous, 4.0)
+            self.assertEqual(
+                maintenance,
+                [2.5, 1.0, 0.1, 0.3, 0.05, 0.02, 0.02, 0.01],
+            )
+            self.assertEqual(maintenance_total, 4.0)
 
 
 if __name__ == '__main__':
